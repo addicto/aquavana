becf53b1c38017a01f923a0d75823198065268e2
atthachet-mac-pro <atthachet@mbc.co.th>
2023-04-25 14:08:38 +0700

    - modify settings for supporting dev environment

--- pages/urls.py
+++ pages/urls.py
@@ -17,9 +17,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
-from aquavana import settings
+from pages import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('pages.urls')),
-] ## + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    path('', views.home, name='home'),
+]
